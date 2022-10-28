7ccaa24eca10a6e7b2faaa02a81cfc1e7fc0afcb
shohayabt <shohayabhasan@yahoo.com>
2022-10-28 07:17:23 +0600

    work with user req

--- index.js
+++ index.js
@@ -17,6 +17,31 @@ app.createServer = () => {
 };
 // Handle Request and Response
 app.handleRequestResponse = (req, res) => {
+    // Request Handle
+    // Get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    // console.log(parsedUrl);
+    const path = parsedUrl.pathname;
+    // console.log(path);
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    // console.log(trimmedPath);
+    const method = req.method.toLowerCase();
+    // console.log(method);
+    const queryStringObject = parsedUrl.query;
+    // console.log(queryStringObject);
+    const headersObject = req.headers;
+    // console.log(headersObject);
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+    });
+
     // Sending a response
     res.end('Hello Programmer ');
 };
